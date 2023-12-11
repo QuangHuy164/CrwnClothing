84fe5227b2cae332008cb5f51e8645f344630b9e
Huy Nguyen Quang <nghuy164@gmail.com>
2023-12-11 04:59:03 +0200

    commit

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -15,12 +15,23 @@ const addCartItem = (cartItems, productToAdd) => {
     }
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+}
  
 export const CartContext = createContext ({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemFromCart: () => {},
+    cartCount: 0,
 });
 
 export const CartProvider = ({children}) => {
@@ -38,6 +49,10 @@ export const CartProvider = ({children}) => {
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
+    };
+
+    const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(addCartItem(cartItems, cartItemToRemove));
     };
     const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
 

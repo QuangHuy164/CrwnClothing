fca3539dc1c4ea529d748d1b07f6e84698964590
QuangHuy164 <nghuy164@gmail.com>
2025-01-17 04:50:27 +0200

    update

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,38 +1,33 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import { CartContext} from '../../contexts/cart.context';
+import { CartContext } from "../../contexts/cart.context";
 
-import './checkout-item.style.scss';
+import "./checkout-item.style.scss";
 
-const CheckoutItem = ({cartItem}) => {
-    const { name, imageUrl, price, quantity,} = cartItem;
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
 
-    const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
+  const { clearItemFromCart, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
 
-    const clearItemHandler = () => clearItemFromCart(cartItem);
+  const clearItemHandler = () => clearItemFromCart(cartItem);
 
-    return (
-        <div className='checkout-item-container'>
-            <div className='image-container'>
-                <img src={imageUrl} alt={`${name}`} />
-            </div>
-            <span className='name'> {name}</span>
-            <span className='quantity'>
-                <div className='arrow'>
+  return (
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={`${name}`} />
+      </div>
+      <span className="name"> {name}</span>
+      <span className="quantity">
+        <div className="arrow"></div>
+        {quantity}
+        <div className="arrow"></div>
+      </span>
 
-            </div>
-                {quantity}
-                <div className='arrow'>
-
-            </div>
-                </span>
-            
-            
-            <span className='price'> {price}</span>
-            <button className='remove-button' onClick={clearItemHandler}>
-                &#10005;
-                </button>
-        </div>
-        
-    )
-}
+      <span className="price"> {price}</span>
+      <button className="remove-button" onClick={clearItemHandler}>
+        &#10005;
+      </button>
+    </div>
+  );
+};

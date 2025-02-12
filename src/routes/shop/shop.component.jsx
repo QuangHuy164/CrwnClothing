518effe57a2fafd8e5b6b9b70a31330cd9377ba1
QuangHuy164 <nghuy164@gmail.com>
2025-02-12 20:34:52 +0200

    update

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,6 +2,7 @@ import { useContext, Fragment } from "react";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 import "./shop.styles.scss";
 

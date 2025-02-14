de06c822a997dfda1dddfb1c00b4568c57d0877e
QuangHuy164 <nghuy164@gmail.com>
2025-02-14 06:44:15 +0200

    update

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,16 @@
-import { useContext, Fragment } from "react";
+import {Routes, Route} from 'react-router-dom'
 
-import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component'
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const {categoriesMap} = useContext(CategoriesContext);
+ 
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Routes>
+      <Route index element={< CategoriesPreview />} />
+      <Route path=":category" element={< Category />} />
+    </Routes>
   );
 };
 

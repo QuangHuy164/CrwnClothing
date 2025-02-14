bed16ca80c0263c9129da096906fb49c117030dc
QuangHuy164 <nghuy164@gmail.com>
2025-02-15 01:45:09 +0200

    update

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,24 +1,30 @@
-import { useContext, useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import ProductCard from '../../components/product-card/product-card.component'
+import { useContext, useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
+import ProductCard from "../../components/product-card/product-card.component";
 
-import './category.style.scss'
-import { CategoriesContext } from '../../contexts/categories.context'
+import "./category.style.scss";
+import { CategoriesContext } from "../../contexts/categories.context";
 
 const Category = () => {
-    const {category} = useParams()
-    const {categoriesMap} = useContext(CategoriesContext)
-    const [products, setProducts] = useState(categoriesMap[category])
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
 
-    useEffect(()=> {
-        setProducts(categoriesMap[category])
-    }, [category,categoriesMap])
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
-    return <div className='category-container'>
-        {products && 
-            products.map((product)=> <ProductCard key={product.id} product={product} />)}
-    </div>
+  return (
+    <Fragment className="title">
+      {category.toUpperCase()}
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
+  );
+};
 
-}
-   
-export default Category
+export default Category;

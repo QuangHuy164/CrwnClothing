e4ae20230eb8cc8d09244ed6ed89e3cd7ce15299
QuangHuy164 <nghuy164@gmail.com>
2025-03-03 18:28:00 +0200

    update

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,13 +7,14 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
+import {useSelector} from 'react-redux'
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import './navigation.styles.scss';
+import {NavigationContainer, NavLinks, NavLinks,LogoContainer} from './navigation.styles';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext); 
+  useSelector((state) => state.user.currentUser)
   const { isCartOpen } = useContext(CartContext);
 
     return (

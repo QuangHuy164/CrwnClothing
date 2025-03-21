75843f56524a68c448612a206c7eba0145c74250
QuangHuy164 <nghuy164@gmail.com>
2025-03-22 01:00:51 +0200

    update

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,6 +1,9 @@
 import { CATEGORIES_ACTION_TYPES, Category, CategoryItem } from "./category.types"
 import { createAction } from "../../utils/reducer/reducer.utils"
-import getCategoriesAndDocuments from '../../utils/firebase/firebase.utils'
+import { Action } from "redux"
+
+export type fetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>
+
 
 export const setCategories = (categoriesArray) => createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray)
 

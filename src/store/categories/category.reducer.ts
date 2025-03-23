3eb3b66e3d58c2fd53e0888c1d39949228c37a09
QuangHuy164 <nghuy164@gmail.com>
2025-03-23 22:31:55 +0200

    update

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -7,14 +7,13 @@ export const CATEGORIES_INITIAL_STATE = {
 }
 
 export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {} as CategoryAction) => {
-    const {type, payload} = action;
-    switch (type) {
+    switch (action.type) {
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
             return {...state, isLoading: true}
         case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-            return {...state, categories: payload, isLoading: false}
+            return {...state, categories: action.payload, isLoading: false}
             case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-                return {...state, error: payload, isLoading: false}
+                return {...state, error: action.payload, isLoading: false}
         default:
             return state
     }

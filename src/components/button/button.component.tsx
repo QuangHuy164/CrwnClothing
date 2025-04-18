bb38b03f8b76c1cdf5bd5e5588d3c30bc1a4e3d4
QuangHuy164 <nghuy164@gmail.com>
2025-04-18 23:44:54 +0300

    upadte

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,29 @@
+import {BaseButton,
+    GoogleSignInButton,
+    InvertedButton,
+    LoadingSpinner,} from './button.styles';
+
+export enum BUTTON_TYPE_CLASSES {
+    base = 'base',
+    google = 'google-sign-in',
+    inverted = 'inverted',
+}
+
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton => 
+({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+}[buttonType])
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+    return ( 
+    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} 
+        {...otherProps}
+    >
+        {children}
+    </button>
+    );
+};
+
+export default Button;

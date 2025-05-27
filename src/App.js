0933be5f7638f6fb072c43ef0280813bec0e24e1
QuangHuy164 <nghuy164@gmail.com>
2025-05-27 04:17:46 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import {useDispatch} from 'react-redux'
 import { Routes, Route } from 'react-router-dom';
 
 import { 
     onAuthStateChangedListener, createUserDocumentFromAuth,
 } from "./utils/firebase/firebase.utils";
-import Navigation from './routes/navigation/navigation.component';
-import Home from './routes/home/home.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
 import {setCurrentUser} from './store/user/user.action'
+
+const Navigation = lazy(() => import('./routes/navigation/navigation.component')) ;
+const Authentication = lazy(() => import('./routes/authentication/authentication.component'))
+const Shop = lazy(() => import('./routes/shop/shop.component')) ;
+const Checkout = lazy(() => import('./routes/checkout/checkout.component')) ;
+const Home = lazy(()=> import('./routes/home/home.component'))
 
 const App = () => {
   const dispatch = useDispatch()
@@ -24,6 +25,7 @@ const App = () => {
         return unSubcribe;
       }, []);
   return ( 
+    <Suspense>
     <Routes>
       <Route path='/' element={<Navigation/>}>
       <Route index element={<Home />} />
@@ -32,6 +34,7 @@ const App = () => {
       <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
+    </Suspense>
   );
 };
 
